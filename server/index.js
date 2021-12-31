4454a5de1567f557b937fde106eb24ef66c253b4
pushkalmani <47297313+pushkalmani@users.noreply.github.com>
2021-12-31 11:27:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://pushkal:Idontknow%40125@cluster0.sfpbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://pushkal:<password>@cluster0.sfpbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
